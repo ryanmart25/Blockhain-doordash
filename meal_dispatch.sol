98b9a01f1799b058ba7740994e9c5b08a7a879a4
Mojtaba Karanizadeh <146018430+skaranizadeh@users.noreply.github.com>
2025-11-07 14:29:52 -0800

    order struct updated, mappings, events added

--- meal_dispatch.sol
+++ meal_dispatch.sol
@@ -2,6 +2,8 @@
 pragma solidity 0.8.0;
 
 contract MealDispatchDApp {
+	
+	// Enums for order states 
 	enum OrderStatus {
 		Placed,
 		Accepted,
@@ -9,6 +11,7 @@ contract MealDispatchDApp {
 		Completed
 	}
 
+	// Structs for Store, Customer, Driver, and Order
 	struct Store {
 		string name;
 		address accountAddress;
@@ -28,7 +31,35 @@ contract MealDispatchDApp {
 		address customer;
 		address store;
 		address driver;
+		uint foodTotal;
+		uint foodTip;
+		uint deliveryFee;
+		uint deliveryTip;
+		uint totalAmount;
 		OrderStatus status;
 	}
 
+	// Mappings to store entities and orders
+
+	// Mapping to link store names from saved stores (IPFS) to Store structs
+	mapping(string => Store) public stores;
+
+	// Mapping to link customer names from saved customers (IPFS) to Customer structs
+	mapping(string => Customer) public customers;
+
+	// Mapping to link driver names from saved drivers (IPFS) to Driver structs
+	mapping(string => Driver) public drivers;
+
+	// events
+	event OrderPlaced(address indexed customer, address indexed store, uint indexed orderId);
+	event OrderAccepted(address indexed store, uint indexed orderId);
+	event OrderDelivered(address indexed driver, uint indexed orderId);
+	event OrderCompleted(address indexed customer, uint indexed orderId);	
+	event StoreRegistered(string storName, address indexed accountAddress);
+	event CustomerRegistered(string customerName, address indexed accountAddress);
+	event DriverRegistered(string driverName, address indexed accountAddress);
+	event PaymentReceived(address indexed from, uint amount);
+
+	// constructor
+	constructor() {}
 }
